60a690e66dbfb62b53ea065223ee67c7491c9381
Hubert Choiński <hubert.choinski1@unilodz.eu>
2021-05-28 16:53:27 +0200

    changed Condition to string in adsQueries

--- Application/Models/Queries/GetAllAdsQueries.cs
+++ Application/Models/Queries/GetAllAdsQueries.cs
@@ -3,7 +3,7 @@ namespace Application.Models.Queries
 
         public class GetAllAdsQueries
         {
-            public Cond Condition { get; set; }
+            public string Condition { get; set; }
 
             public string CategoryId { get; set; }
 
@@ -16,9 +16,5 @@ namespace Application.Models.Queries
             public string Author { get; set; }
 
             public string Title { get; set; }
-        }
-        public enum Cond
-        {
-            All, New, Used
         }
 }
